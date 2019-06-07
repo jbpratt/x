65e891df4b44d69b21c0a952755d9763cf392dea
jbpratt78 <jbpratt78@gmail.com>
2019-06-07 16:35:13 -0500

    basic priority queue with 3 tables

--- lua/autumn/lib/algorithms.lua
+++ lua/autumn/lib/algorithms.lua
@@ -44,7 +44,89 @@ function queue()
   return out
 end
 
+function pqueue()
+  local t = {}
+
+  -- set of elements
+  local set = {}
+  local r_set = {}
+  local keys = {}
+
+  local function addKV(k, v)
+    set[k] = v
+    if not r_set[v] then
+      ti(keys,v)
+      table.sort(keys)
+      local k0 = {k}
+      r_set[v] = k0
+      setmetatable(k0,{
+          __mode = 'v'
+        })
+    else
+      ti(r_set[v],k)
+    end
+  end
+
+  local remove = function(k)
+    local v = set[k]
+    local prioritySet = r_set[v]
+    tr2(prioritySet,k)
+    if #prioritySet < 1 then
+      tr2(keys,v)
+      r_set[v] = nil
+      table.sort(keys)
+      set[k] = nil
+    end
+  end; t.remove = remove
+
+  t.min = function()
+    local priority = keys[1]
+    if priority then
+      return r_set[priority][1] or {}
+    else
+      return {}
+    end
+  end
+  
+  t.max = function()
+    local priority = keys[#keys]
+    if priority then
+      return r_set[priority][1] or {}
+    else
+      return {}
+    end
+  end
+
+  t.empty = function()
+    return #keys < 1
+  end
+
+  t.iterate = function()
+    return function()
+      if not t.empty() then
+        local ele = t.max()
+        t.remove(ele)
+        return ele
+      end
+    end
+  end
+
+  setmetatable(t, {
+    __index = set,
+    __newindex = function(t,k,v)
+      if not set[k] then
+        addKV(k,v)
+      else
+        remove(k)
+        addKV(k,v)
+      end
+    end,
+  })
+  return t
+end
+
 return {
   stack = stack,
   queue = queue,
+  pqueue = pqueue,
 }
